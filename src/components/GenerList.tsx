079c9b7b0d267cbf2c51a8369b546dbed1221591
yihune21 <zewdieyihune@gmail.com>
2024-04-11 16:22:23 +0300

    diplay genres

--- src/components/GenerList.tsx
+++ src/components/GenerList.tsx
@@ -1,13 +1,24 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenerList = () => {
   const { data } = useGenres();
   return (
-    <ul>
-      {data.map((gener) => (
-        <li key={gener.id}>{gener.name}</li>
+    <List>
+      {data.map((genre) => (
+        <ListItem key={genre.id} paddingY="5px">
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            />
+            <Text fontSize="lg">{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
